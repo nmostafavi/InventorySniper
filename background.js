30d8e6c1501ca6291382d1e149e07d765264dd01
Neema Mostafavi <github@neema.me>
2020-12-29 02:45:16 -0800

    Added notification toast

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-var listener = function(request, sender, sendResponse)
+var requestListener = function(request, sender, sendResponse)
 {
     if (request["playAlert"])
     {
@@ -6,7 +6,24 @@ var listener = function(request, sender, sendResponse)
         document.body.appendChild(audio);
         audio.autoplay = true;
         audio.src = "braam.mp3";
+
+        chrome.notifications.create("inventoryAvailableNotification", {
+            "type": "basic",
+            "iconUrl": "icon.png",
+            "title": "Added to cart!",
+            "message": "Return to browser to begin checkout.",
+            "requireInteraction": true
+        }, function(notificationId) { /* do nothing */ });
     }
 };
 
-chrome.extension.onRequest.addListener(listener);
+var notificationClickedListener = function(notificationId)
+{
+    chrome.windows.update(chrome.windows.WINDOW_ID_CURRENT, {
+        "focused": true
+    }, function(window) { /* do nothing */ });
+    chrome.notifications.clear(notificationId);
+};
+
+chrome.extension.onRequest.addListener(requestListener);
+chrome.notifications.onClicked.addListener(notificationClickedListener);
