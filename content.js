0c0a5989cab2bdfd8c9b956577434cc0293cdf0f
Neema Mostafavi <github@neema.me>
2020-12-28 17:20:22 -0800

    Refactored a bit to simplify the flow. Now mostly everything is in one simple function.

--- content.js
+++ content.js
@@ -1,43 +1,36 @@
 const timeout = 20000;  // How long to wait for the page to fully load, in milliseconds
 const reloadInterval = 10000;  // How long to wait before reloading when item is deemed out of stock, in milliseconds
 
-var attemptAddToCart = function()
-{
-    // Check for a "Pick it up" or "Pick up here" button:
-    //   "Pick it up" will appear if the item is available at your selected store.
-    //   "Pick up here" will appear if the item is available at another nearby store.
-    //   Both of these are the same button, but with different text applied.
-    var orderPickupButton = document.querySelector("button[data-test=orderPickupButton");
-    if (orderPickupButton)
-    {
-        orderPickupButton.click();
-        chrome.extension.sendRequest({"playAlert": true});
-        return;
-    }
-
-    // Could not find the buy button, assume out of stock
-    setTimeout(function() { location.reload(); }, reloadInterval);
-}
-
 var runCheck = function() 
 {
-    // Wait until the inventory widget populates
+    // Poll until the inventory widget populates
     const startTime = new Date();
     const pollInterval = 100;  // milliseconds
     var checkInventoryWidgetLoaded = function()
     {
-        if (document.querySelector("div[data-test=flexible-fulfillment]"))
+        var orderPickupButton = document.querySelector("button[data-test=orderPickupButton");
+        if (orderPickupButton)
         {
-            // Widget loaded
-            attemptAddToCart();
+            alert("in stock! adding to cart");
+            orderPickupButton.click();
+            chrome.extension.sendRequest({"playAlert": true});
+            return;
+        }
+
+        var outOfStockMessage = document.querySelector('div[data-test=outOfStockMessage]');
+        if (outOfStockMessage)
+        {
+            alert("out of stock");
+            setTimeout(function() { location.reload(); }, reloadInterval);
             return;
         }
 
         var currentTime = new Date();
         if (currentTime - startTime > timeout)
         {
-            // Timed out waiting for the widget to appear
+            // Timed out waiting for the page to finish loading (or it's not eligible for pickup)
             location.reload();
+            return;
         }
 
         setTimeout(checkInventoryWidgetLoaded, pollInterval);
